f137378572d034d57ba124d3fc5ab22c87449406
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-04-03 15:25:36 +0200

    cleaned up namespaces

--- StockExchange/src/Presentation.Console/Program.cs
+++ StockExchange/src/Presentation.Console/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Presentation.Console
+﻿namespace Presentation.Console
 {
     public class Program
     {
