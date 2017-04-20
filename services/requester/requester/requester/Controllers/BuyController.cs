7ee2b4d06d3396eb29cc5b252161c53efd9966a6
Joachim <joachimdamandersen@gmail.com>
2017-04-20 18:13:18 +0200

    implemeted BuyOrder Class

--- services/requester/requester/requester/Controllers/BuyController.cs
+++ services/requester/requester/requester/Controllers/BuyController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using requester.Models;
 
 namespace requester.Controllers
 {
     [Route("[Controller]")]
-    public class ValuesController : Controller
+    public class BuyController : Controller
     {
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]int buyerId, string tickerSymbol, int maxPrice, int quantity)
+        public void Post([FromBody] BuyOrder buyOrder)
         {
 
         }
--- services/requester/requester/requester/Models/BuyOrder.cs
+++ services/requester/requester/requester/Models/BuyOrder.cs
@@ -7,5 +7,9 @@ namespace requester.Models
 {
     public class BuyOrder
     {
+        public int BuyerId { get; set; }
+        public string TickerSymbol { get; set; }
+        public int MaxPrice { get; set; }
+        public int Quantity { get; set; }
     }
 }
