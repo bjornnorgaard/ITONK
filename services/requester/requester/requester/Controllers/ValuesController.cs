00fceecbd63deff25bff22f3c226f044b51d0d33
Joachim <joachimdamandersen@gmail.com>
2017-04-20 17:49:15 +0200

    Created .NET core requester service

---
+++ services/requester/requester/requester/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace requester.Controllers
+{
+    [Route("[Controller]")]
+    public class ValuesController : Controller
+    {
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]int buyerId, string tickerSymbol, int maxPrice, int quantity)
+        {
+
+        }
+    }
+}
