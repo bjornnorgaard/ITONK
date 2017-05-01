e26697c15ffb72fe93ed1f7b057474c19e9d1338
Martin Carpentier <martin@carpentier.dk>
2017-05-01 08:04:32 +0200

    Added tax calculations and tax office stub
    Added tax calculations and tax office stub

--- services/taxtobin/TaxTobin/TaxTobin/NancyModules/TaxTobinModule.cs
+++ services/taxtobin/TaxTobin/TaxTobin/NancyModules/TaxTobinModule.cs
@@ -8,16 +8,23 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using TaxTobin.DataLink;
 using TaxTobin.Models;
+using TaxTobin.TaxCalculations;
 
 namespace TaxTobin.NancyModules
 {
     public class TaxTobinModule : NancyModule
     {
         private ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private TaxCalculator taxCalculator;
+        private ExternalTaxSystemComm taxSystemComm;
 
         public TaxTobinModule() : base(string.Empty)
         {
+            taxCalculator = new TaxCalculator();
+            taxSystemComm = new ExternalTaxSystemComm();
+
             Get["/Test"] = parameters =>
             {
                 var feeds = new string[] { "foo", "bar" };
@@ -31,12 +38,21 @@ namespace TaxTobin.NancyModules
                 {
                     var saleInfo = this.Bind<SaleInfo>();
 
+                    //Setting seller id to the logging context. This helps identifying the statements
+                    log4net.GlobalContext.Properties["sellerId"] = saleInfo.sellerId;
+
                     log.Info("Received following request:");
 
                     log.Info("ID: " + saleInfo.sellerId);
                     log.Info("TotalPrice_: " + saleInfo.totalPrice);
 
-                    return HttpStatusCode.OK;
+                    double taxedTotalPrice = taxCalculator.CalculateTax(saleInfo);
+
+                    if(taxSystemComm.SendToTaxGovernments(saleInfo, taxedTotalPrice))
+                    {
+                        return HttpStatusCode.OK;
+                    }
+                    return HttpStatusCode.InternalServerError;
                 }
                 catch(Exception e)
                 {
