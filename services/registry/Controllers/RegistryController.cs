64d5f19938b6c5791ab882c20ce3ac7ec2afb1e6
dda <dda@kamstrup.com>
2017-05-04 11:22:14 +0200

    Implemented ChangeOwnerShip

--- services/registry/Controllers/RegistryController.cs
+++ services/registry/Controllers/RegistryController.cs
@@ -1,7 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
 using Registry.Models;
+using Registry.Models.ViewModel;
 
 namespace Registry.Controllers
 {
@@ -16,7 +19,7 @@ namespace Registry.Controllers
         }
 
         [HttpGet]
-        public IActionResult checkOwnership(string tickerSymbol, int sellerId, int quantity)
+        public IActionResult CheckOwnership(string tickerSymbol, int sellerId, int quantity)
         {
             if (string.IsNullOrWhiteSpace(tickerSymbol) || sellerId == 0 || quantity == 0)
             {
@@ -29,9 +32,32 @@ namespace Registry.Controllers
         }
 
         [HttpPost]
-        public void changeOwnership([FromBody] JObject jsonbody)
+        public IActionResult ChangeOwnership([FromBody] JObject jsonbody)
         {
+            BuyViewModel shareViewModel = jsonbody.ToObject<BuyViewModel>();
 
+            if (shareViewModel.BuyerId == 0 || shareViewModel.Quantity == 0 || shareViewModel.SellerId == 0 || string.IsNullOrWhiteSpace(shareViewModel.TickerSymbol))
+            {
+                Response.StatusCode = 400;
+                return Json(new { errorMessage = "Insufficient parameters given" });
+            }
+            
+            var shares = context.Shares.Where(share => share.Owner == shareViewModel.SellerId && share.TickerSymbol == shareViewModel.TickerSymbol);
+
+            if (shares.Count() < shareViewModel.Quantity)
+                return Json(new { errorMessage = "Insufficient shares owned" });
+
+            var sharesToBeSold = shares.Take(shareViewModel.Quantity);
+
+            foreach (var share in sharesToBeSold)
+            {
+                share.Owner = shareViewModel.BuyerId;
+            }
+
+            context.Shares.UpdateRange(sharesToBeSold);
+            context.SaveChanges();
+
+            return Json(new { Message = "Shares sold" });
         }
     }
 }
---
+++ services/registry/Models/ViewModel/BuyViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Registry.Models.ViewModel
+{
+    public class BuyViewModel
+    {
+        public int SellerId { get; set; }
+        public int Quantity { get; set; }
+        public int BuyerId { get; set; }
+        public string TickerSymbol { get; set; }
+    }
+}
