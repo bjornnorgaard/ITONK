a42ed5f1d1efdfe979351fa67e747b52d139c08e
dda <dda@kamstrup.com>
2017-05-04 10:25:46 +0200

    Added check ownership

--- services/registry/Controllers/RegistryController.cs
+++ services/registry/Controllers/RegistryController.cs
@@ -13,22 +13,28 @@ namespace Registry.Controllers
     public class RegistryController : Controller
     {
         private readonly ShareContext context;
+
         public RegistryController(ShareContext shareContext)
         {
             this.context = shareContext;
         }
         [HttpGet]
-        public string checkOwnership(string tickerSymbol, int sellerId, int quantity)
+        public IActionResult checkOwnership(string tickerSymbol, int sellerId, int quantity)
         {
-            
+            if (string.IsNullOrWhiteSpace(tickerSymbol) || sellerId == 0 || quantity == 0)
+            {
+                Response.StatusCode = 400;
+                return Json(new { errorMessage = "Insufficient parameters given" });
+            }
+            var shares = context.Shares.Where(share => share.Owner == sellerId && share.TickerSymbol == tickerSymbol);
 
-            return "value";
+            return Json(shares.Count() >= quantity ? new { Owner = "True" } : new { Owner = "False" });
         }
 
-        // POST api/values
         [HttpPost]
         public void changeOwnership([FromBody] JObject jsonbody)
         {
+
         }
     }
 }
