e3aab42655f34d0675b7115f82b3274123a4b760
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-05-04 12:29:55 +0200

    Using MockBrokerService in controller

--- services/provider/Provider/Provider/Startup.cs
+++ services/provider/Provider/Provider/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Services;
+using Services.Mocks;
 
 namespace Provider
 {
