87a1893ba836ebe2fa073d098944ff699acd2416
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-05-08 15:13:18 +0200

    Added domain models for buying and selling stuff

--- services/broker/Broker/Interfaces/Class1.cs
+++ services/broker/Broker/Interfaces/Class1.cs
@@ -2,7 +2,5 @@
 
 namespace Interfaces
 {
-    public class Class1
-    {
-    }
+
 }
---
+++ services/broker/Broker/Models/SellOrder.cs
@@ -0,0 +1,10 @@
+﻿namespace Models
+{
+    public class SellOrder
+    {
+        public int SellerId { get; set; }
+        public string TickerSymbol { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
