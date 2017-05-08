7c2111b3bbb7a8228bc67999ae432d26a72ea913
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-05-08 16:26:14 +0200

    Created first migration

---
+++ services/broker/Broker/Broker/Migrations/20170508142059_Initial.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Broker.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    BuyOrderId = table.Column<int>(nullable: false),
+                    SellOrderId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BuyOrders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    BuyerId = table.Column<int>(nullable: false),
+                    MaxPrice = table.Column<int>(nullable: false),
+                    Quantity = table.Column<int>(nullable: false),
+                    TickerSymbol = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BuyOrders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SellOrders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Price = table.Column<int>(nullable: false),
+                    Quantity = table.Column<int>(nullable: false),
+                    SellerId = table.Column<int>(nullable: false),
+                    TickerSymbol = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SellOrders", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "BuyOrders");
+
+            migrationBuilder.DropTable(
+                name: "SellOrders");
+        }
+    }
+}
---
+++ services/broker/Broker/Broker/Migrations/BrokerContextModelSnapshot.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Broker.Models;
+
+namespace Broker.Migrations
+{
+    [DbContext(typeof(BrokerContext))]
+    partial class BrokerContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("Broker.Models.Transaction", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("BuyOrderId");
+
+                    b.Property<int>("SellOrderId");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Transactions");
+                });
+
+            modelBuilder.Entity("Models.BuyOrder", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("BuyerId");
+
+                    b.Property<int>("MaxPrice");
+
+                    b.Property<int>("Quantity");
+
+                    b.Property<string>("TickerSymbol");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("BuyOrders");
+                });
+
+            modelBuilder.Entity("Models.SellOrder", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("Price");
+
+                    b.Property<int>("Quantity");
+
+                    b.Property<int>("SellerId");
+
+                    b.Property<string>("TickerSymbol");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("SellOrders");
+                });
+        }
+    }
+}
