bd0e481687c58d2d362ef7468241f408fe9b17c9
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-05-08 15:32:29 +0200

    Created db context with additional model

---
+++ services/broker/Broker/DataAccess/BrokerContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace DataAccess
+{
+    public class BrokerContext : DbContext
+    {
+        public BrokerContext(DbContextOptions<BrokerContext> options) : base(options) { }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<BuyOrder> BuyOrders { get; set; }
+        public DbSet<SellOrder> SellOrders { get; set; }
+    }
+}
---
+++ services/broker/Broker/DataAccess/Transaction.cs
@@ -0,0 +1,9 @@
+namespace DataAccess
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public int BuyOrderId { get; set; }
+        public int SellOrderId { get; set; }
+    }
+}
