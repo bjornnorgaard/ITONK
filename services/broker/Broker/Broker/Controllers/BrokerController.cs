ff823cf6ced76d13f3410cefdfd38b2fe3e3b19d
Bjorn Nørgaard <bjornsnorgaard@gmail.com>
2017-05-15 23:14:22 +0200

    Untested implementation of BrokerController

--- services/broker/Broker/Broker/Controllers/BrokerController.cs
+++ services/broker/Broker/Broker/Controllers/BrokerController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Broker.DbModels;
 using Interfaces;
@@ -23,9 +22,45 @@ namespace Broker.Controllers
         }
 
         [HttpPost]
-        public Task<IActionResult> Buy([FromBody]BuyOrder value)
+        public async Task<IActionResult> Buy([FromBody]BuyOrder buyOrder)
         {
-            throw new NotImplementedException();
+            var buyRecord = SaveOrderToDatabase(buyOrder);
+            var sellRecord = FindMatchingOrder(buyRecord);
+
+            if (sellRecord == null)
+            {
+                Response.StatusCode = 202;
+                return Json(new { status = "No match found. Will buy later." });
+            }
+
+            var changeOwnershipObject = new ChangeOwnershipObject
+            {
+                BuyerId = buyRecord.BuyerId,
+                Quantity = sellRecord.Quantity,
+                SellerId = sellRecord.SellerId,
+                TickerSymbol = sellRecord.TickerSymbol
+            };
+            if (await _registryService.ChangeOwnershipAsync(changeOwnershipObject) == false)
+            {
+                Response.StatusCode = 503;
+                return Json(new { status = "Could not change ownership." });
+            }
+
+            UpdateRecordsOnMatch(sellRecord, buyRecord);
+
+            var taxNotifyObject = new TaxNotifyObject
+            {
+                SellerId = sellRecord.SellerId,
+                TotalPrice = sellRecord.Price * sellRecord.Quantity
+            };
+            if (await _taxService.InformTaxTobin(taxNotifyObject) == false)
+            {
+                Response.StatusCode = 503;
+                return Json(new { status = "Tax was not applied." });
+            }
+
+            Response.StatusCode = 201;
+            return Json(new { status = "Match found. Was bought." });
         }
 
         [HttpPost]
@@ -33,69 +68,115 @@ namespace Broker.Controllers
         {
             if (await _registryService.IsValidOwnershipAsync(sellOrder) == false)
             {
-                Response.StatusCode = 200;
-                return Json(new { status = "Shit aint valid" });
+                Response.StatusCode = 204;
+                return Json(new { status = "Shit ain't valid" });
             }
 
-            var sellRecord = new SellRecord
+            var sellRecord = SaveOrderToDatabase(sellOrder);
+            var buyRecord = FindMatchingOrder(sellRecord);
+
+            if (buyRecord == null)
             {
-                SellerId = sellOrder.SellerId,
-                Price = sellOrder.Price,
-                Quantity = sellOrder.Quantity,
-                TickerSymbol = sellOrder.TickerSymbol
-            };
-
-            _context.SellRecords.Add(sellRecord);
-            _context.SaveChanges();
-
-            var matchingBuyOrders = _context.BuyRecords
-                .Where(b => b.TickerSymbol == sellRecord.TickerSymbol)
-                .Where(b => b.Quantity >= sellRecord.Quantity)
-                .First(b => b.MaxPrice >= sellRecord.Price);
-
-            if (matchingBuyOrders == null)
-            {
-                Response.StatusCode = 200;
+                Response.StatusCode = 202;
                 return Json(new { status = "No match found. Will be sold later." });
             }
 
             var changeOwnershipObject = new ChangeOwnershipObject
             {
-                BuyerId = matchingBuyOrders.BuyerId,
+                BuyerId = buyRecord.BuyerId,
                 Quantity = sellRecord.Quantity,
                 SellerId = sellRecord.SellerId,
                 TickerSymbol = sellRecord.TickerSymbol
             };
-
             if (await _registryService.ChangeOwnershipAsync(changeOwnershipObject) == false)
             {
-                Response.StatusCode = 200;
+                Response.StatusCode = 503;
                 return Json(new { status = "Could not change ownership." });
             }
 
-            var transaction = new Transaction
-            {
-                BuyOrderId = matchingBuyOrders.BuyerId,
-                SellOrderId = sellRecord.SellerId
-            };
-
-            _context.Transactions.Add(transaction);
-            _context.SaveChanges();
+            UpdateRecordsOnMatch(sellRecord, buyRecord);
 
             var taxNotifyObject = new TaxNotifyObject
             {
                 SellerId = sellRecord.SellerId,
                 TotalPrice = sellRecord.Price * sellRecord.Quantity
             };
-
-            if (await _taxService.InformTaxGuy(taxNotifyObject) == false)
+            if (await _taxService.InformTaxTobin(taxNotifyObject) == false)
             {
-                Response.StatusCode = 200;
+                Response.StatusCode = 503;
                 return Json(new { status = "Tax was not applied." });
             }
 
-            Response.StatusCode = 200;
+            Response.StatusCode = 201;
             return Json(new { status = "Match found. Was sold." });
+        }
+
+        private void UpdateRecordsOnMatch(SellRecord sellRecord, BuyRecord buyRecord)
+        {
+            sellRecord.IsSold = true;
+            buyRecord.IsBought = true;
+
+            var transaction = new Transaction
+            {
+                BuyRecordId = buyRecord.Id,
+                SellRecordId = sellRecord.Id
+            };
+
+            _context.Update(sellRecord);
+            _context.Update(buyRecord);
+            _context.Transactions.Add(transaction);
+
+            _context.SaveChanges();
+        }
+
+        private BuyRecord FindMatchingOrder(SellRecord sellRecord)
+        {
+            var findMatchingBuyOrder = _context.BuyRecords
+                .Where(b => b.TickerSymbol == sellRecord.TickerSymbol)
+                .Where(b => b.Quantity >= sellRecord.Quantity)
+                .First(b => b.MaxPrice >= sellRecord.Price);
+
+            return findMatchingBuyOrder;
+        }
+        private SellRecord FindMatchingOrder(BuyRecord buyRecord)
+        {
+            var findMatchingBuyOrder = _context.SellRecords
+                .Where(s => s.TickerSymbol == buyRecord.TickerSymbol)
+                .Where(s => s.Quantity >= buyRecord.Quantity)
+                .First(s => s.Price <= buyRecord.MaxPrice);
+
+            return findMatchingBuyOrder;
+        }
+
+        private SellRecord SaveOrderToDatabase(SellOrder order)
+        {
+            var sellRecord = new SellRecord
+            {
+                SellerId = order.SellerId,
+                Price = order.Price,
+                Quantity = order.Quantity,
+                TickerSymbol = order.TickerSymbol
+            };
+
+            _context.SellRecords.Add(sellRecord);
+            _context.SaveChanges();
+
+            return sellRecord;
+        }
+        private BuyRecord SaveOrderToDatabase(BuyOrder order)
+        {
+            var buyRecord = new BuyRecord()
+            {
+                BuyerId= order.BuyerId,
+                MaxPrice = order.MaxPrice,
+                Quantity = order.Quantity,
+                TickerSymbol = order.TickerSymbol
+            };
+
+            _context.BuyRecords.Add(buyRecord);
+            _context.SaveChanges();
+
+            return buyRecord;
         }
     }
 }
