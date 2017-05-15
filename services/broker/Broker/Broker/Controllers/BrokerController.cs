5f4d5810d714649dc7c95ae3ed2af879303b0e7c
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-05-15 15:14:32 +0200

    Started impl of Tax- and RegistryService

--- services/broker/Broker/Broker/Controllers/BrokerController.cs
+++ services/broker/Broker/Broker/Controllers/BrokerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Models;
 
@@ -8,6 +9,15 @@ namespace Broker.Controllers
     [Route("[controller]/[action]")]
     public class BrokerController : Controller
     {
+        private readonly IRegistryService _registryService;
+        private readonly ITaxService _taxService;
+
+        public BrokerController(IRegistryService registryService, ITaxService taxService )
+        {
+            _registryService = registryService;
+            _taxService = taxService;
+        }
+
         [HttpPost]
         public Task<IActionResult> Buy([FromBody]BuyOrder value)
         {
--- services/broker/Broker/Services/TaxService.cs
+++ services/broker/Broker/Services/TaxService.cs
@@ -1,10 +1,29 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using Interfaces;
 
 namespace Services
 {
     public class TaxService : ITaxService
     {
+        public static HttpClient Client { get; set; }
+
+        public TaxService(string taxApiAddress)
+        {
+            if (string.IsNullOrWhiteSpace(taxApiAddress))
+            {
+                throw new ArgumentException($"The argument {nameof(taxApiAddress)} was null or whitespace. " +
+                                            $"Please fill in the relevant sections in 'appsettings.json'. " +
+                                            $"The key should be called something like {nameof(taxApiAddress)}.");
+            }
+
+            Client = new HttpClient { BaseAddress = new Uri(taxApiAddress) };
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
         public Task<bool> InformTaxGuy(int d)
         {
             throw new System.NotImplementedException();
