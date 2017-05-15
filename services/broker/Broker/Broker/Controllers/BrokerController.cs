1f4aae587abde468f98aae44f5bbef5efc26b252
Bjorn Nørgaard <bjornsnorgaard@gmail.com>
2017-05-15 22:19:10 +0200

    Wip on BrokerController

--- services/broker/Broker/Broker/Controllers/BrokerController.cs
+++ services/broker/Broker/Broker/Controllers/BrokerController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Broker.DbModels;
 using Interfaces;
@@ -28,43 +29,73 @@ namespace Broker.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Sell([FromBody]SellOrder value)
+        public async Task<IActionResult> Sell([FromBody]SellOrder sellOrder)
         {
-            if (await _registryService.IsValidOwnershipAsync(value) == false)
+            if (await _registryService.IsValidOwnershipAsync(sellOrder) == false)
             {
                 Response.StatusCode = 200;
                 return Json(new { status = "Shit aint valid" });
             }
 
-            await _context.SellOrders.AddAsync(value);
+            var sellRecord = new SellRecord
+            {
+                SellerId = sellOrder.SellerId,
+                Price = sellOrder.Price,
+                Quantity = sellOrder.Quantity,
+                TickerSymbol = sellOrder.TickerSymbol
+            };
 
-            var isMatchFound = await FindMatchAsync(value);
+            _context.SellRecords.Add(sellRecord);
+            _context.SaveChanges();
 
-            if (isMatchFound == false)
+            var matchingBuyOrders = _context.BuyRecords
+                .Where(b => b.TickerSymbol == sellRecord.TickerSymbol)
+                .Where(b => b.Quantity >= sellRecord.Quantity)
+                .First(b => b.MaxPrice >= sellRecord.Price);
+
+            if (matchingBuyOrders == null)
             {
                 Response.StatusCode = 200;
                 return Json(new { status = "No match found. Will be sold later." });
             }
-            if (await _registryService.ChangeOwnership(new ChangeOwnershipObject()) == false)
+
+            var changeOwnershipObject = new ChangeOwnershipObject
+            {
+                BuyerId = matchingBuyOrders.BuyerId,
+                Quantity = sellRecord.Quantity,
+                SellerId = sellRecord.SellerId,
+                TickerSymbol = sellRecord.TickerSymbol
+            };
+
+            if (await _registryService.ChangeOwnershipAsync(changeOwnershipObject) == false)
             {
                 Response.StatusCode = 200;
                 return Json(new { status = "Could not change ownership." });
             }
-            if (await _taxService.InformTaxGuy(new TaxNotifyObject()) == false)
+
+            var transaction = new Transaction
+            {
+                BuyOrderId = matchingBuyOrders.BuyerId,
+                SellOrderId = sellRecord.SellerId
+            };
+
+            _context.Transactions.Add(transaction);
+            _context.SaveChanges();
+
+            var taxNotifyObject = new TaxNotifyObject
+            {
+                SellerId = sellRecord.SellerId,
+                TotalPrice = sellRecord.Price * sellRecord.Quantity
+            };
+
+            if (await _taxService.InformTaxGuy(taxNotifyObject) == false)
             {
                 Response.StatusCode = 200;
                 return Json(new { status = "Tax was not applied." });
             }
 
-            // Update broker db.
-
             Response.StatusCode = 200;
             return Json(new { status = "Match found. Was sold." });
-        }
-
-        private Task<bool> FindMatchAsync(SellOrder sellOrder)
-        {
-            throw new NotImplementedException();
         }
     }
 }
