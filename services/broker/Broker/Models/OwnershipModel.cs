086abcae79614e6e506c99facd91e0d122463099
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-05-15 16:53:13 +0200

    Reorders properties

--- services/broker/Broker/Models/OwnershipModel.cs
+++ services/broker/Broker/Models/OwnershipModel.cs
@@ -3,8 +3,8 @@
     public class OwnershipModel
     {
         public int SellerId { get; set; }
-        public int Quantity { get; set; }
         public int BuyerId { get; set; }
+        public int Quantity { get; set; }
         public string TickerSymbol { get; set; }
     }
 }
