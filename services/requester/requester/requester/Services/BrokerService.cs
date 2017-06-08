a2fa88904848450c0d09787f79a7303422bd39fe
Dennis Tychsen <dty@kamstrup.com>
2017-06-08 12:20:08 +0200

    Fix jokke noob program

--- services/requester/requester/requester/Services/BrokerService.cs
+++ services/requester/requester/requester/Services/BrokerService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using requester.Models;
@@ -19,7 +20,7 @@ namespace requester.Services
             HttpClient.DefaultRequestHeaders.Accept.Clear();
             HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             var srlzd = JsonConvert.SerializeObject(buyOrder);
-            var httpOrder = new StringContent(srlzd);
+            var httpOrder = new StringContent(srlzd, Encoding.UTF8, "application/json");
             var httpResponse = await HttpClient.PostAsync("/buy", httpOrder);
             return httpResponse;
         }
